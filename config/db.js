75f7a91eeb4e443c204eda7e33653544d2e3c44a
Partho <daspartho710@gmail.com>
2026-09-18 17:14:22 +0600

    Completed Module 1: Authentication (Register, Login, Logout)

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '2203082630645',
+  database: 'banking_system',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
